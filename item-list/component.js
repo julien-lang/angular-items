2044f7d0c3a08c4ba8554ec877a76c08e8b56b17
Julien Langlois <julien.langlois@gmail.com>
2017-06-03 10:29:04 -0700

    Introduce http data loader

--- item-list/component.js
+++ item-list/component.js
@@ -1,12 +1,13 @@
 angular.module('itemsApp').component('itemList', {
 	templateUrl: 'item-list/template.html',
-	controller: function() {
-		this.records = [
-			{ name: 'item 1', url: "http://my-test.com/1", type:"Type-1", status:"OK", include: false },
-			{ name: 'item-2', url: "http://my-test.com/2", type:"Type-1", status:"Error", include: false },
-			{ name: 'item-2', url: "http://my-test.com/2", type:"Type-1", status:"Processing", include: false },
-			{ name: 'item-2', url: "http://my-test.com/2", type:"Type-1", status:"Offline", include: false }
-		];
+	controller: function($http) {
+		this.records = [];
+		var self = this;
+		
+		$http.get('item-list/data.json').then(function(response) {
+			self.records = response.data;
+		});
+		
 		// Delete data
 		this.Delete = function (index) {
 			this.records.splice(index, 1);
