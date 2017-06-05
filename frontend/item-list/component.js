2f9673a4d385f545549c7635eb90ce50fefdb3e2
Julien Langlois <julien.langlois@gmail.com>
2017-06-04 17:59:53 -0700

    Finish Crud

--- frontend/item-list/component.js
+++ frontend/item-list/component.js
@@ -1,41 +1,49 @@
 angular.module('itemsApp').component('itemList', {
 	templateUrl: 'item-list/template.html',
-	controller: ['$http', 'Item', function($http, Item) {
-		this.items = Item.query();
+	controller: ['$http', '$interval', 'Item', function($http, $interval, Item) {
+		var self = this;
+		this.api_list = function(){
+			self.items = Item.query();
+		};
+		
+		$interval(self.api_list, 10000);
+		this.api_list();
 		
 		this.delete_item = function (item_id) {
-			console.log("delete "+item_id);
-			
+			var self = this;
 			$http.get('/api/delete/' + item_id).then(function(response) {
 				if (response.data["status"] == "deleted") {
-					self.items = Item.query();
+					delete self.items[item_id];
 				}
 			});
 		};
+		
+		// Reset new data model
+		this.Reset = function (form) {
+			form.newName = '';
+			form.newURL = "";
+			form.newType = 0;
+		};
+		
+		// Add new data
+		this.Add = function (form) {
+			var data = {
+				"name": form.newName,
+				"type": form.newType
+			};
+			
+			if (form.newURL) {
+				data["url"] = form.newURL
+			}
+			
+			this.Reset(form);
+			
+			var self = this;
+			$http.post('/api/new', data).then(function(response) {
+				if (response.data["status"] == "Processing") {
+					self.items[response.data["id"]] = response.data;
+				}
+			});
+		}
 	}],
-		// // Delete data
-		// this.Delete = function (index) {
-		// 	this.items.splice(index, 1);
-		// };
-		// // Reset new data model
-		// this.Reset = function (form) {
-		// 	form.newName = '';
-		// 	form.newURL = "";
-		// 	form.newType = 0;
-		// }
-		// // this.Reset();
-		// // Add new data
-		// this.Add = function (form) {
-		// 	// Add to main items
-		// 	this.items.push({
-		// 		name: form.newName,
-		// 		type: form.newType,
-		// 		url: form.newURL,
-		// 		status: "Processing",
-		// 		include: false
-		// 	});
-		// 	// See this.Reset...
-		// 	this.Reset(form);
-		// }
-	// }
 });
